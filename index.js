3cef4899e693f064642e78d8ebf2ddb5ed92695a
geoorgeqq <grajdeanu.george@yahoo.ro>
2023-10-28 19:23:23 +0300

    Update JS
    - Rezolvat bug ( nu se accesa pagina question4.html)

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 let btn = document.querySelectorAll(".btn");
-let intrebari = ["Question2.html", "Question3.html","Question4.html", "Final.html"];
+let intrebari = ["Question2.html", "Question3.html", "Question4.html","Final.html"];
 currentIndex = 0;
 
-btn.forEach((btn,currentIndex) => {
+btn.forEach((btn, currentIndex) => {
     btn.addEventListener("click", () => {
         if (btn.getAttribute("value") === "corect" && currentIndex < intrebari.length) {
-            window.location = intrebari[currentIndex];
+            window.location.href = intrebari[currentIndex];
         }else {
             btn.style.backgroundColor = "red";
         }
     });
+    
 });
 
 
