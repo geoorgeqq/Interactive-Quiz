2584891656833835b9934beb59125e613252d9c3
geoorgeqq <grajdeanu.george@yahoo.ro>
2023-10-28 19:03:35 +0300

    Proiect Updated
    - Adaugat inca 3 intrebari
    - La raspuns corect treci la urmatoarea pagina HTML
    - Adaugat buton de prima pagina

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 let btn = document.querySelectorAll(".btn");
+let intrebari = ["Question2.html", "Question3.html","Question4.html", "Final.html"];
+currentIndex = 0;
 
-btn.forEach(btn => {
-    btn.addEventListener("click", () =>{
-        if(btn.getAttribute("value") === "corect"){
-            alert("Corect!");
-        }else{
+btn.forEach((btn,currentIndex) => {
+    btn.addEventListener("click", () => {
+        if (btn.getAttribute("value") === "corect" && currentIndex < intrebari.length) {
+            window.location = intrebari[currentIndex];
+        }else {
             btn.style.backgroundColor = "red";
         }
-    })
-    
+    });
 });
 
 
